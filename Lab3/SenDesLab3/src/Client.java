4ce7dce7f3ff4ab0f7d28a48bc90a3398684c86d
Gavin <gbannerman@uiowa.edu>
2015-12-10 12:22:39 -0600

    Edited client

--- Lab3/SenDesLab3/src/Client.java
+++ Lab3/SenDesLab3/src/Client.java
@@ -1,6 +1,4 @@
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
@@ -11,16 +9,13 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class Client extends JFrame implements Runnable{
-	private static final long serialVersionUID = 1826871141583020576L;
 	private String clientHost;	//host name for server
 	private Socket connection; // connection to server
 	private ObjectInputStream input; // input from server
